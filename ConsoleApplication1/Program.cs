730a7c200b9112851683f94b9aae589ebc1103af
Koraneg <roman.dr96@gmail.com>
2017-12-21 14:48:17 +0300

    Создание первого класса

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            char sym;
+            x=3;
+            y = 4;
+            sym = '%';
+            for (int i=0;i<20;i++)
+            {
+                for(int j=0;j<20;j++)
+                {
+                    Draw(i,j,sym);
+                    j++;
+                }
+                i++;
+                i++;
+            }
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+
+}
